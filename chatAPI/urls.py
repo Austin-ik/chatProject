e2fa3e59481218bdcc94cd716bcc81a1cd819b81
Ikhide Augustine <augustineimoleme@gmail.com>
2024-02-02 20:34:36 +0100

    new update

--- chatAPI/urls.py
+++
@@ -1,9 +0,0 @@
-# chat_app/urls.py
-
-from django.urls import path
-from .views import SendMessageView
-
-urlpatterns = [
-    path('send-message/', SendMessageView.as_view(), name='send-message'),
-    # Add other API endpoints as needed
-]
